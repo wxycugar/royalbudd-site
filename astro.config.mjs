a7b92059fe5726883e53a7475a98ad3c43d813d5
wxycugar <wangxinyan1490@gmail.com>
2026-04-10 22:43:21 +0800

    feat: 接入 Sanity API，实现首页 Hero 区域数据动态渲染

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import sanity from '@sanity/astro';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  integrations: [
+    sanity({
+      projectId: '518irhg4', // <--- 注意：请把你在后端 sanity.cli.ts 里找到的那串字母填在这里，保留单引号！
+      dataset: 'production',
+      useCdn: false, // 设为 false，确保每次刷新都能看到最新传的图
+      apiVersion: '2024-04-10', // 接口版本日期
+    })
+  ]
 });
